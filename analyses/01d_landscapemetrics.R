ac21f92d03de0f965b4d6a2a38210af348bd85a9
RomainMon <monassier_romain@hotmail.fr>
2025-09-08 18:22:14 -0300

    Creation of the landscape metrics script to upload land cover data (MapBiomas), prepare the data, and compute landscape metrics

---
+++ analyses/01d_landscapemetrics.R
@@ -0,0 +1,88 @@
+#------------------------------------------------#
+# Author: Romain Monassier
+# Objective: Compute landscape metrics
+#------------------------------------------------#
+
+### Load packages ------
+library(dplyr)
+library(tidyr)
+library(here)
+library(ggplot2)
+library(landscapemetrics)
+library(terra)
+
+### Import data -------
+## Rasters
+base_path <- here("data", "geo", "MapBiomas", "colecao_9") 
+raster_files <- list.files(base_path, pattern = "\\.tif$", full.names = TRUE)                  
+rasters <- lapply(raster_files, terra::rast)
+# Shapefile
+base_path <- here("data", "geo", "IBGE", "admin", "BR_UF_2023")
+region = vect(file.path(base_path, "RJ_state.shp"))
+
+### Quick check -------
+crs(rasters[[1]])
+crs(region)
+ext(rasters[[1]])
+ext(region)
+plot(rasters[[1]])  # plot the first layer
+plot(region)
+
+### Crop rasters ------
+
+## Reproject shapefile to match raster CRS
+region <- project(region, crs(rasters[[1]]))
+
+## Quick check
+plot(rasters[[1]])  
+plot(region, add = TRUE)
+
+## Using the RJ state boundaries as a layer
+rasters_cropped <- lapply(rasters, function(r) {
+  r_cropped <- crop(r, region) # crop returns a geographic subset of an object as specified by an Extent object
+  r_masked <- mask(r_cropped, region) # create a new Raster object that has the same values as x, except for the cells that are NA in a 'mask' (either a Raster or a Spatial object)
+  return(r_masked)
+})
+
+# Check one
+plot(rasters_cropped[[1]])
+plot(region, add = TRUE, border = "red")
+
+### Reproject --------
+# Target CRS
+target_crs <- "EPSG:31983"
+
+rasters_reprojected <- lapply(rasters_cropped, function(r) {
+  # Use method = "near" for categorical data (like MapBiomas land cover)
+  project(r, target_crs, method = "near")
+})
+
+# Quick check
+crs(rasters_reprojected[[1]])
+plot(rasters_reprojected[[1]])
+
+# Export rasters
+# Define output folder
+output_dir <- here("outputs", "data", "MapBiomas")
+
+# Export each raster
+for (i in seq_along(rasters_reprojected)) {
+  r <- rasters_reprojected[[i]]
+  
+  # Construct a filename using the original raster name
+  file_name <- paste0(names(r)[1], "_RJ_state_31983.tif")
+  output_path <- file.path(output_dir, file_name)
+  
+  # Write raster to file
+  writeRaster(r, output_path, overwrite = TRUE)
+}
+
+### Compute landscape metrics ---------
+# NB: All functions in landscapemetrics start with lsm_ (for landscape metrics). The second part of the name specifies the level (patch - p, class - c or landscape - l)
+
+## Convert your  raster to a categorical raster and focus on value = 3 ("Forest formation" in the 9th MapBiomas collection)
+# NB: landscapemetrics works on categorical rasters where classes are integers
+
+## Summary statistics at the landscape scale
+
+## Summary statistics at the patch level
