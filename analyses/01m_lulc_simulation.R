f1cbea3d7b41afe429fa78dd46a59e0cc028ee21
RomainMon <monassier_romain@hotmail.fr>
2026-02-26 17:06:59 +0100

    Stack static predictors into a ExpVar object (lulcc)

--- analyses/01m_lulc_simulation.R
+++ analyses/01m_lulc_simulation.R
@@ -10,6 +10,7 @@ library(ggplot2)
 library(raster)
 library(terra)
 library(lulcc)
+library(landscapemetrics)
 
 ### Import rasters -------
 
@@ -23,7 +24,7 @@ years_lulc = stringr::str_extract(basename(raster_files), "(?<!\\d)\\d{4}(?!\\d)
 raster_df = data.frame(file = raster_files, years_lulc = as.numeric(years_lulc)) %>%
   dplyr::arrange(years_lulc)
 # Load rasters in chronological order
-rasters_reclass = lapply(raster_df$file, raster::raster) # IMPORTANT : import with raster and not terra!
+rasters_reclass = lapply(raster_df$file, terra::rast)
 years_lulc = raster_df$years_lulc
 # Check
 for (i in seq_along(rasters_reclass)) {
@@ -42,7 +43,7 @@ years_tm = stringr::str_extract(basename(raster_files), "(?<!\\d)\\d{4}(?!\\d)")
 raster_df = data.frame(file = raster_files, years_tm = as.numeric(years_tm)) %>%
   dplyr::arrange(years_tm)
 # Load rasters in chronological order
-rasters_tm = lapply(raster_df$file, raster::raster)
+rasters_tm = lapply(raster_df$file, terra::rast)
 years_tm = raster_df$years_tm
 # Check
 for (i in seq_along(rasters_tm)) {
@@ -50,24 +51,6 @@ for (i in seq_along(rasters_tm)) {
 }
 plot(rasters_tm[[35]], col=c("#32a65e", "#ad975a", "#519799", "#FFFFB2", "#0000FF", "#d4271e", "chartreuse", "pink"))
 
-
-## Forest age
-base_path = here("outputs", "data", "MapBiomas", "Rasters_forest_age")
-raster_files = list.files(base_path, pattern = "\\.tif$", full.names = TRUE)
-
-# Extract years
-years_age = stringr::str_extract(basename(raster_files), "(?<!\\d)\\d{4}(?!\\d)")
-# Create a dataframe to link files and years
-raster_df = data.frame(file = raster_files, years_age = as.numeric(years_age)) %>%
-  dplyr::arrange(years_age)
-# Load rasters in chronological order
-rasters_forest_age = lapply(raster_df$file, raster::raster)
-years_age = raster_df$years_age
-# Check
-for (i in seq_along(rasters_forest_age)) {
-  cat("Year", years_age[i], " → raster name:", basename(raster_df$file[i]), "\n")
-}
-plot(rasters_forest_age[[36]])
 
 ## Slope
 slope_r = terra::rast(here("data", "geo", "TOPODATA", "work", "slope_bbox.tif"))
@@ -90,7 +73,7 @@ raster_df = data.frame(file = raster_files, years_climate = as.numeric(years_cli
   dplyr::arrange(years_climate)
 
 # Load rasters in chronological order
-rasters_prec_sum = lapply(raster_df$file, raster::raster)
+rasters_prec_sum = lapply(raster_df$file, terra::rast)
 years_climate = raster_df$years_climate
 # Check
 for (i in seq_along(rasters_prec_sum)) {
@@ -110,7 +93,7 @@ raster_df = data.frame(file = raster_files, years_climate = as.numeric(years_cli
   dplyr::arrange(years_climate)
 
 # Load rasters in chronological order
-rasters_tmin_mean = lapply(raster_df$file, raster::raster)
+rasters_tmin_mean = lapply(raster_df$file, terra::rast)
 years_climate = raster_df$years_climate
 # Check
 for (i in seq_along(rasters_tmin_mean)) {
@@ -130,7 +113,7 @@ raster_df = data.frame(file = raster_files, years_climate = as.numeric(years_cli
   dplyr::arrange(years_climate)
 
 # Load rasters in chronological order
-rasters_tmax_mean = lapply(raster_df$file, raster::raster)
+rasters_tmax_mean = lapply(raster_df$file, terra::rast)
 years_climate = raster_df$years_climate
 # Check
 for (i in seq_along(rasters_tmax_mean)) {
@@ -148,7 +131,7 @@ car_sf = sf::st_as_sf(car)
 
 # Mutate area and id
 car_sf = car_sf %>% dplyr::mutate(car_id = row_number(),
-                                  car_area_m2 = round(as.numeric(st_area(geometry)),2),
+                                  car_area_m2 = round(as.numeric(sf::st_area(geometry)),2),
                                   car_area_ha = round((car_area_m2 / 10000), 2))
 
 ## CAR (Reserva Legal)
@@ -172,7 +155,7 @@ roads_sf = sf::st_as_sf(roads)
 br101 = roads_sf %>% 
   dplyr::filter(ref == "BR-101")
 plot(rasters_reclass[[36]], col=c("#32a65e", "#ad975a", "#519799", "#FFFFB2", "#0000FF", "#d4271e"))
-plot(st_geometry(br101), col="#ff3399", add=TRUE)
+plot(sf::st_geometry(br101), col="#ff3399", add=TRUE)
 
 ## Rivers
 rivers = terra::vect(here("data", "geo", "OSM", "work", "Waterway_OSM_AMLD_clean.shp"))
@@ -215,7 +198,456 @@ pub_res_sf = sf::st_as_sf(pub_res)
 plot(pub_res)
 
 
+### Create Explanatory Variables Raster List ------
+# EV are represented by an object of class ExpVarRasterList
+# Can be either static (one map provided for the study period) or dynamic (one map provided for each year of the study period)
+
+#### Create rasters -------
+# Select template raster to create other rasters
+template_rast = rasters_reclass[[1]]
+plot(template_rast, col=c("#32a65e", "#ad975a", "#519799", "#FFFFB2", "#0000FF", "#d4271e"))
+names(rasters_reclass) = years_lulc
+
+##### Legal status -------
+## Rasterize data
+# Private properties
+car_r = terra::rasterize(car_sf, template_rast, field = 1, background = 0)
+plot(car_r)
+# Public reserve
+pub_res_r = terra::rasterize(pub_res_sf, template_rast, field = 1, background = 0)
+plot(pub_res_r)
+# RPPN
+rppn_r = terra::rasterize(rppn_sf, template_rast, field = 1, background = 0)
+plot(rppn_r)
+# Reserva legal
+rl_r = terra::rasterize(rl_sf, template_rast, field = 1, background = 0)
+plot(rl_r)
+# APA
+apa_r = terra::rasterize(apa_mld_sf, template_rast, field = 1, background = 0)
+plot(apa_r)
+
+##### Distances -------
+# Rivers (static)
+rivers_bin = terra::rasterize(rivers_sf, template_rast, field = 1, background = NA)
+plot(rivers_bin, col="purple")
+dist_rivers_r = terra::distance(rivers_bin)
+plot(dist_rivers_r)
+
+## Urban centers (static)
+urban_bin = terra::rasterize(urb_sf, template_rast, field = 1, background = NA)
+plot(urban_bin, col="purple")
+dist_urban_r = terra::distance(urban_bin)
+plot(dist_urban_r)
+
+## Roads (dynamic)
+dist_roads_list = list()
+# Loop over years
+for (yr in years_lulc) {
+  # Keep roads created that year or before
+  roads_sub = roads_sf[roads_sf$date_crea <= yr, ]
+  
+  # Check if roads exist
+  if (nrow(roads_sub) == 0) {
+    raster_list[[as.character(yr)]] = NA
+    next
+  }
+  
+  # Rasterize roads
+  roads_bin = terra::rasterize(roads_sub, template_rast, field = 1, background = NA)
+  
+  # Compute distance
+  dist_r = terra::distance(roads_bin)
+  
+  # Store the raster into a list
+  dist_roads_list[[as.character(yr)]] = dist_r
+}
+plot(dist_roads_list[[1]])
+plot(dist_roads_list[[36]])
+
+## Forest edges (dynamic)
+dist_edges_list =list()  # Liste pour stocker les rasters de distance aux bords
+
+# Boucle sur les années
+for (yr in years_lulc) {
+  cat("  Processing forest edges for year:", yr, "\n")
+  
+  # LULC raster for a given year
+  r_year = rasters_reclass[[as.character(yr)]]
+  
+  # Create a binary mask (forest = 1, the rest = NA)
+  forest_mask = r_year
+  forest_mask[forest_mask != 1] = NA
+  
+  # Compute forest edges
+  edge_list = landscapemetrics::get_boundaries(forest_mask, as_NA = TRUE)
+  edge_rast = edge_list[[1]]
+  
+  # Compute distance
+  dist_edge_r = terra::distance(edge_rast)
+  
+  # Store the raster
+  dist_edges_list[[as.character(yr)]] = dist_edge_r
+}
+plot(dist_edges_list[[1]])
+plot(dist_edges_list[[36]])
+
+
+##### LULC in a buffer -------
+
+## Forest
+# Initialisation
+prop_forest100m_list = list()  # List to store rasters
+
+# Parameters
+cl = 1  # Land use of interest
+radius = 100 # Radius in meters
+
+for (yr in years_lulc) {
+  cat("  Processing LULC proportion for year:", yr, "\n")
+  
+  # Raster of a given year
+  r_year = rasters_reclass[[as.character(yr)]]
+  
+  # Create binary mask
+  r_bin = r_year == cl
+  
+  # Create a circular focal matrix using the radius parameter
+  w = terra::focalMat(r_bin, type = "circle", d = radius, fillNA = TRUE)
+  
+  # Count pixels in the neighborhood
+  focal_count = terra::focal(r_bin, w = w, fun = "sum", na.rm = TRUE)
+  
+  # Count valid pixels (not NAs) in the neighborhood
+  r_valid = !is.na(r_year)
+  focal_valid = terra::focal(r_valid, w = w, fun = "sum", na.rm = TRUE)
+  
+  # Compute the proportion of LULC
+  prop = focal_count / focal_valid
+  prop[is.infinite(prop) | is.na(prop)] = NA
+  
+  # Store the result in a list
+  prop_forest100m_list[[as.character(yr)]] = prop
+}
+plot(prop_forest100m_list[[1]])
+plot(prop_forest100m_list[[36]])
+
+## Agriculture
+# Initialisation
+prop_agri100m_list = list()  # List to store rasters
+
+# Parameters
+cl = 4  # Land use of interest
+radius = 100 # Radius in meters
+
+for (yr in years_lulc) {
+  cat("  Processing LULC proportion for year:", yr, "\n")
+  
+  # Raster of a given year
+  r_year = rasters_reclass[[as.character(yr)]]
+  
+  # Create binary mask
+  r_bin = r_year == cl
+  
+  # Create a circular focal matrix using the radius parameter
+  w = terra::focalMat(r_bin, type = "circle", d = radius, fillNA = TRUE)
+  
+  # Count pixels in the neighborhood
+  focal_count = terra::focal(r_bin, w = w, fun = "sum", na.rm = TRUE)
+  
+  # Count valid pixels (not NAs) in the neighborhood
+  r_valid = !is.na(r_year)
+  focal_valid = terra::focal(r_valid, w = w, fun = "sum", na.rm = TRUE)
+  
+  # Compute the proportion of LULC
+  prop = focal_count / focal_valid
+  prop[is.infinite(prop) | is.na(prop)] = NA
+  
+  # Store the result in a list
+  prop_agri100m_list[[as.character(yr)]] = prop
+}
+plot(prop_agri100m_list[[1]])
+plot(prop_agri100m_list[[36]])
+
+## Urban
+# Initialisation
+prop_urban100m_list = list()  # List to store rasters
+
+# Parameters
+cl = 6  # Land use of interest
+radius = 100 # Radius in meters
+
+for (yr in years_lulc) {
+  cat("  Processing LULC proportion for year:", yr, "\n")
+  
+  # Raster of a given year
+  r_year = rasters_reclass[[as.character(yr)]]
+  
+  # Create binary mask
+  r_bin = r_year == cl
+  
+  # Create a circular focal matrix using the radius parameter
+  w = terra::focalMat(r_bin, type = "circle", d = radius, fillNA = TRUE)
+  
+  # Count pixels in the neighborhood
+  focal_count = terra::focal(r_bin, w = w, fun = "sum", na.rm = TRUE)
+  
+  # Count valid pixels (not NAs) in the neighborhood
+  r_valid = !is.na(r_year)
+  focal_valid = terra::focal(r_valid, w = w, fun = "sum", na.rm = TRUE)
+  
+  # Compute the proportion of LULC
+  prop = focal_count / focal_valid
+  prop[is.infinite(prop) | is.na(prop)] = NA
+  
+  # Store the result in a list
+  prop_urban100m_list[[as.character(yr)]] = prop
+}
+plot(prop_urban100m_list[[1]])
+plot(prop_urban100m_list[[36]])
+
+
+##### North and South of BR101 -------
+br_coords = sf::st_coordinates(br101)  # x and y coordinates of BR-1010
+
+# Create an empty raster
+ns_raster = terra::rast(template_rast)
+ns_raster[] = NA 
+
+# Determine whether a point is North or South of located coords
+compute_ns = function(x, y, br_coords) {
+  # Find nearest coord (in longitude, x)
+  idx = which.min(abs(br_coords[, 1] - x))
+  br_y = br_coords[idx, 2] # Y coord of the nearest BR-101 point
+  
+  # Compare Y with the BR-101 Y coordinate
+  if (y > br_y) {
+    return(0)  # North
+  } else {
+    return(1)  # South
+  }
+}
+
+# Apply to all the raster
+# We transform the raster to data frame
+raster_df = as.data.frame(ns_raster, xy = TRUE, na.rm = FALSE)
+
+# Apply the function to each X, Y point
+raster_df$ns = mapply(
+  compute_ns,
+  x = raster_df$x,
+  y = raster_df$y,
+  MoreArgs = list(br_coords = br_coords)
+)
+
+# Rebuild raster with 0 (North) and 1 (South)
+ns_raster[] = raster_df$ns
+
+# Plot
+plot(ns_raster, main = "Nord (0) / Sud (1) de la BR-101", col = c("blue", "red"))
+
+
+
+#### Transform to RasterLayer objects -----
+# We must transform each terra Raster to a raster Raster
+# lulcc works with raster objects (RasterLayer)
+
+##### Static rasters -------
+slope_r = raster::raster(slope_r)
+topo_r = raster::raster(topo_r)
+apa_r = raster::raster(apa_r)
+rl_r = raster::raster(rl_r)
+car_r = raster::raster(car_r)
+rppn_r = raster::raster(rppn_r)
+pub_res_r = raster::raster(pub_res_r)
+dist_urban_r = raster::raster(dist_urban_r)
+dist_rivers_r = raster::raster(dist_rivers_r)
+ns_raster = raster::raster(ns_raster)
+
+##### Dynamic rasters -compareRaster()##### Dynamic rasters ------
+
+# Precipitations
+prec_list = list()
+# Loop
+for (i in seq_along(rasters_prec_sum)) { # Change "rasters_prec_sum" by the list of terra rasters
+  # Extract SpatRaster (terra object)
+  spat_raster = rasters_prec_sum[[i]]
+  
+  # ... to RasterLayer (raster object)
+  raster_layer = raster::raster(spat_raster)
+  
+  # Name raster with the corresponding year
+  year = years_lulc[i]  # Replace "years_lulc" by the vector of years
+  raster_name = paste0("prec_", year) # Replace by what we name the raster
+  
+  # Store the raster
+  prec_list[[raster_name]] = raster_layer
+}
+# Check
+plot(prec_list$prec_1989)
+head(freq(prec_list$prec_1989))
+head(freq(rasters_prec_sum[[1]]))
+head(freq(prec_list$prec_2024))
+head(freq(rasters_prec_sum[[36]]))
+
+# Tmin
+tmin_list = list()
+# Loop
+for (i in seq_along(rasters_tmin_mean)) {
+  # Extract SpatRaster (terra object)
+  spat_raster = rasters_tmin_mean[[i]]
+  
+  # ... to RasterLayer (raster object)
+  raster_layer = raster::raster(spat_raster)
+  
+  # Name raster with the corresponding year
+  year = years_lulc[i]  # Replace "years_lulc" by the vector of years
+  raster_name = paste0("tmin_", year) # Replace by what we name the raster
+  
+  # Store the raster
+  tmin_list[[raster_name]] = raster_layer
+}
+
+# Tmax
+tmax_list = list()
+# Loop
+for (i in seq_along(rasters_tmax_mean)) {
+  # Extract SpatRaster (terra object)
+  spat_raster = rasters_tmax_mean[[i]]
+  
+  # ... to RasterLayer (raster object)
+  raster_layer = raster::raster(spat_raster)
+  
+  # Name raster with the corresponding year
+  year = years_lulc[i]  # Replace "years_lulc" by the vector of years
+  raster_name = paste0("tmax_", year) # Replace by what we name the raster
+  
+  # Store the raster
+  tmax_list[[raster_name]] = raster_layer
+}
+
+# Distances to roads
+dist_roads_list_v2 = list()
+# Loop
+for (i in seq_along(dist_roads_list)) {
+  # Extract SpatRaster (terra object)
+  spat_raster = dist_roads_list[[i]]
+  
+  # ... to RasterLayer (raster object)
+  raster_layer = raster::raster(spat_raster)
+  
+  # Name raster with the corresponding year
+  year = years_lulc[i]  # Replace "years_lulc" by the vector of years
+  raster_name = paste0("dist_road_", year) # Replace by what we name the raster
+  
+  # Store the raster
+  dist_roads_list_v2[[raster_name]] = raster_layer
+}
+
+# Distances to forest edges
+dist_edges_list_v2 = list()
+# Loop
+for (i in seq_along(dist_edges_list)) {
+  # Extract SpatRaster (terra object)
+  spat_raster = dist_edges_list[[i]]
+  
+  # ... to RasterLayer (raster object)
+  raster_layer = raster::raster(spat_raster)
+  
+  # Name raster with the corresponding year
+  year = years_lulc[i]  # Replace "years_lulc" by the vector of years
+  raster_name = paste0("dist_edge_", year) # Replace by what we name the raster
+  
+  # Store the raster
+  dist_edges_list_v2[[raster_name]] = raster_layer
+}
+# Check
+plot(dist_edges_list_v2$dist_edge_2024)
+head(freq(dist_edges_list$`2024`))
+head(freq(dist_edges_list_v2$dist_edge_2024))
+
+# LULC in buffers
+# Forest
+prop_forest100m_list_v2 = list()
+# Loop
+for (i in seq_along(prop_forest100m_list)) {
+  # Extract SpatRaster (terra object)
+  spat_raster = prop_forest100m_list[[i]]
+  
+  # ... to RasterLayer (raster object)
+  raster_layer = raster::raster(spat_raster)
+  
+  # Name raster with the corresponding year
+  year = years_lulc[i]  # Replace "years_lulc" by the vector of years
+  raster_name = paste0("prop_forest_", year) # Replace by what we name the raster
+  
+  # Store the raster
+  prop_forest100m_list_v2[[raster_name]] = raster_layer
+}
+
+# Agriculture
+prop_agri100m_list_v2 = list()
+# Loop
+for (i in seq_along(prop_agri100m_list)) {
+  # Extract SpatRaster (terra object)
+  spat_raster = prop_agri100m_list[[i]]
+  
+  # ... to RasterLayer (raster object)
+  raster_layer = raster::raster(spat_raster)
+  
+  # Name raster with the corresponding year
+  year = years_lulc[i]  # Replace "years_lulc" by the vector of years
+  raster_name = paste0("prop_agri_", year) # Replace by what we name the raster
+  
+  # Store the raster
+  prop_agri100m_list_v2[[raster_name]] = raster_layer
+}
+
+# Urban
+prop_urban100m_list_v2 = list()
+# Loop
+for (i in seq_along(prop_urban100m_list)) {
+  # Extract SpatRaster (terra object)
+  spat_raster = prop_urban100m_list[[i]]
+  
+  # ... to RasterLayer (raster object)
+  raster_layer = raster::raster(spat_raster)
+  
+  # Name raster with the corresponding year
+  year = years_lulc[i]  # Replace "years_lulc" by the vector of years
+  raster_name = paste0("prop_urban_", year) # Replace by what we name the raster
+  
+  # Store the raster
+  prop_urban100m_list_v2[[raster_name]] = raster_layer
+}
+
+
+#### Compile predictors -----
+# Maps of different explanatory variables should have the same coordinate reference system but do not have to have the same extent and resolution as long as the minimum extent is that of the study region defined by an ObsLulcRasterStack object. 
+# However, maps for different timesteps of the same dynamic variable should have the same extent and resolution because these are stored as RasterStack objects.
+
+## Static predictors
+# First, we compile predictors
+ef = list(
+  ef_001 = apa_r,
+  ef_002 = rl_r,
+  ef_003 = car_r,
+  ef_004 = rppn_r,
+  ef_005 = pub_res_r,
+  ef_006 = dist_urban_r,
+  ef_007 = dist_rivers_r,
+  ef_008 = ns_raster,
+  ef_009 = topo_r,
+  ef_010 = slope_r)
+
+# Then, we create an ExpVarRasterList object
+ef_stack = lulcc::ExpVarRasterList(x = ef, pattern = "ef")
+
+
 ### Create LULC Raster List ------
+
+# lulcc needs RasterLayer objects (raster package) and not SpatRaster (terra)
+
+
 # Name each land use raster
 names(rasters_reclass) = paste0("lu_", years_lulc) # Ex: "lu_1989", "lu_1990", etc.
 rasters_reclass = rasters_reclass[order(years_lulc)]
@@ -234,46 +666,3 @@ obs = lulcc::ObsLulcRasterStack(x=rasters_reclass,
 
 ### Transition matrix -------
 lulcc::crossTabulate(x=obs, times=c(0,35))
-
-### Create Explanatory Variables Raster List ------
-# EV are represented by an object of class ExpVarRasterList
-# Can be either static (one map provided for the study period) or dynamic (one map provided for each year of the study period)
-
-#### Create rasters -------
-template_rast = rasters_reclass[[1]]
-plot(template_rast)
-
-##### Legal status -------
-## Rasterize data
-# Private properties
-car_r = raster::rasterize(car_sf, template_rast, field = 1, background = 0)
-plot(car_r)
-# Public reserve
-pub_res_r = raster::rasterize(pub_res_sf, template_rast, field = 1, background = 0)
-plot(pub_res_r)
-# RPPN
-rppn_r = raster::rasterize(rppn_sf, template_rast, field = 1, background = 0)
-plot(rppn_r)
-# Reserva legal
-rl_r = raster::rasterize(rl_sf, template_rast, field = 1, background = 0)
-plot(rl_r)
-# APA
-apa_r = raster::rasterize(apa_mld_sf, template_rast, field = 1, background = 0)
-plot(apa_r)
-
-##### Distances -------
-# Rivers
-rivers_bin = raster::rasterize(rivers_sf, template_rast, field = 1, background = NA)
-plot(rivers_bin, col="purple")
-dist_rivers_r = raster::distance(rivers_bin)
-plot(dist_rivers_r)
-
-## Urban centers
-urban_bin = terra::rasterize(urb_sf, template_rast, field = 1, background = NA)
-plot(urban_bin, col="purple")
-dist_urban_r = terra::distance(urban_bin)
-plot(dist_urban_r)
-
-##### LULC in a buffer -------
-
-##### North and South of BR101 -------
